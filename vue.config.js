ebed097ca23a461a5561b864a59cce17bc2c0065
minh_bon <quangminh2082002@gmail.com>
2023-11-29 16:42:29 +0700

    update vue.config.js

--- vue.config.js
+++ vue.config.js
@@ -1,8 +1,9 @@
-// const { defineConfig } = require('@vue/cli-service')
-// module.exports = defineConfig({
-//   transpileDependencies: true
-// })
-
-module.exports = {
+const { defineConfig } = require('@vue/cli-service')
+module.exports = defineConfig({
+  transpileDependencies: true,
   publicPath: process.env.NODE_ENV === "production" ? "/poke-memories/" : "/",
-};
+});
+
+// module.exports = {
+//   publicPath: process.env.NODE_ENV === "production" ? "/poke-memories/" : "/",
+// };
